9a694d35eac60f593b42447e86e00ae9bb00891b
mami uchida <uchida-mami3695@g.ecc.u-tokyo.ac.jp>
2021-02-05 15:15:37 +0900

    login画面など変更

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'semantic-ui-react';
 import Navbar from './base/Navbar';
 import '../css/Login.css';
+import { Alert } from 'rsuite';
+import { useHistory } from 'react-router-dom';
+
 
 export default function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = () => {
+        try {
+            if(email.length==0) return Alert.warning('メールアドレスが未入力です。');
+            if(password.length==0) return Alert.warning('パスワードが未入力です。');
+        } catch (err) {
+            Alert.error('ログインに失敗しました。');
+        }
+    }
+
     return (
         <div>
         <Navbar />
@@ -12,10 +28,10 @@ export default function Login() {
                     <p className="Header">Login</p>
                     <Form className="FormContents">
                     <p　className="Labels">メールアドレス</p>
-                    <Form.Input type='email' placeholder='email' className="Input" />
+                    <Form.Input type='email' placeholder='email' className="Input" value={email} onChange={e => setEmail(e.target.value)}/>
                     <p　className="Labels">パスワード</p>
-                    <Form.Input type='password' placeholder='password' className="Input" />
-                    <Button inverted color='yellow' className = "LoginButton" type='submit'>Login</Button>
+                    <Form.Input type='password' placeholder='password' className="Input" value={password} onChange={e => setPassword(e.target.value)}/>
+                    <Button inverted color='yellow' className = "LoginButton" type='submit' onClick={handleSubmit} >Login</Button>
                     </Form>
                 </div>
             </div>
--- src/components/base/Navbar.js
+++ src/components/base/Navbar.js
@@ -4,37 +4,17 @@ import { Link, withRouter } from 'react-router-dom';
 
 function Navbar(){
 
-    const colorsA = ['red', 'orange', 'yellow', 'olive', 'green', 'teal']
-    const [activeA, setColor] = useState(colorsA[0]);
-    // state = { activeA: colorsA[0], activeB: colorsB[0] }
-
-
-    // handleAClick = (e, { name }) => this.setState({ activeA: name })
-    // const { activeA} = this.state
-    // value={email} onChange={e => setEmail(e.target.value)}
     return (
         <>
-        <Menu style={{ width: "100vw", display: "flex", flexDirection: "row", alignItems: "center", marginBottom: "-1px" ,background:"black"}}>
+        <Menu style={{ width: "100vw", display: "flex", flexDirection: "row",justifyContent: "space-around",alignItems: "center", marginBottom: "-1px" ,background:"black"}}>
         <div style={{ fontSize:"0.9em",display: "flex", flexDirection: "row", justifyContent: "space-evenly", width: "80vw"}}>
-            <Link to="/" style={{ color:"#FDDF02", fontWeight:"bold"}} >Home</Link>
+            <Link to="/" style={{ color:"#FDDF02", fontWeight:"bold"}}>Home</Link>
             <Link to="/login" style={{ color:"#FDDF02", fontWeight:"bold"}} >Login</Link>
             <Link to="/signup" style={{ color:"#FDDF02", fontWeight:"bold"}} >Sign up</Link>
             <Link to="/inputs" style={{ color:"#FDDF02", fontWeight:"bold"}} >寄付記録作成</Link>
             <Link to="/donation_list" style={{ color:"#FDDF02", fontWeight:"bold"}} >寄付記録</Link>
         </div>
         </Menu>
-        {/* <Menu inverted>
-          {colorsA.map((c) => (
-            <Menu.Item
-              key={c}
-              name={c}
-              active={activeA === c}
-              color={c}
-              onClick = {(c)=> {setColor(c.target.classList[0])
-              }}
-            />
-          ))}
-        </Menu> */}
         </>
     )
 }
